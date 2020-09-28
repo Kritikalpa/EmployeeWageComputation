5ca996779ce5a79c3ff95fcde732896bdedfc8f7
Kriti Kalpa <krtklpsh@gmail.com>
2020-09-28 11:21:07 +0530

    [Kriti] Add. Part time employee wage calculator

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -6,15 +6,20 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
 
             int empHours = 0;
             int empWage = 0;
             Random random = new Random();
 
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
+            {
+                empHours = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
             {
                 empHours = 8;
             }
