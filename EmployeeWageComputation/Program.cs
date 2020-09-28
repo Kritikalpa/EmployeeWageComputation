fa9bcb67bfe87c57d3f252aa574f48ace0c67bad
Kriti Kalpa <krtklpsh@gmail.com>
2020-09-28 11:27:20 +0530

    [Kriti] Add. Monthly Wage calculator

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -4,35 +4,41 @@ namespace EmployeeWage
 {
     class Program
     {
-
         static void Main(string[] args)
         {
             const int IS_PART_TIME = 1;
             const int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
+            int NUM_OF_WORKING_DAYS = 20;
+            int empHours, empWage, empCheck;
+            int totalWage = 0;
 
-            int empHours = 0;
-            int empWage = 0;
-            Random random = new Random();
-
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            for (int current = 0; current < NUM_OF_WORKING_DAYS; current++)
             {
-                case IS_PART_TIME:
-                    empHours = 4;
-                    break;
+                empHours = 0;
+                empWage = 0;
+                Random random = new Random();
+                empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHours = 4;
+                        break;
 
-                case IS_FULL_TIME:
-                    empHours = 8;
-                    break;
+                    case IS_FULL_TIME:
+                        empHours = 8;
+                        break;
 
-                default:
-                    empHours = 0;
-                    break;
+                    default:
+                        empHours = 0;
+                        break;
+                }
+                empWage = empHours * EMP_RATE_PER_HOUR;
+                totalWage += empWage;
             }
 
-            empWage = empHours * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage : " + empWage);
+
+            Console.WriteLine("Emp Wage : " + totalWage);
         }
     }
 }
