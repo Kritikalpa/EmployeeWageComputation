ff4f3905289d6a2b55f7b8d93fe8b40848a07ac2
Kriti Kalpa <krtklpsh@gmail.com>
2020-09-28 11:14:00 +0530

    [Kriti] Add. Employee Attendence status

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 
-namespace EmployeeWageComputation
+namespace EmployeeWage
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Present");
+            }
+            else
+            {
+                Console.WriteLine("Absent");
+            }
         }
     }
 }
