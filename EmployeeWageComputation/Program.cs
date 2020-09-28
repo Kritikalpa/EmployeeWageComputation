0f5145d77ef760fe2dbff4e4ab41a9ec3d2a333f
Kriti Kalpa <krtklpsh@gmail.com>
2020-09-28 11:30:19 +0530

    [Kriti] Add. Condition for max hours and max days

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -10,10 +10,11 @@ namespace EmployeeWage
             const int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             int NUM_OF_WORKING_DAYS = 20;
+            int NUM_OF_WORKING_HOURS = 100;
             int empHours, empWage, empCheck;
-            int totalWage = 0;
+            int totalWage = 0, currentDay = 0, currentHours = 0;
 
-            for (int current = 0; current < NUM_OF_WORKING_DAYS; current++)
+            while (currentDay < NUM_OF_WORKING_DAYS && currentHours < NUM_OF_WORKING_HOURS)
             {
                 empHours = 0;
                 empWage = 0;
@@ -33,8 +34,11 @@ namespace EmployeeWage
                         empHours = 0;
                         break;
                 }
+                currentHours += empHours;
+                currentDay += 1;
                 empWage = empHours * EMP_RATE_PER_HOUR;
                 totalWage += empWage;
+                Console.WriteLine("Day: " + currentDay + ", Hours worked: " + currentHours + ", Wage received: " + empWage + ", Total Wage: " + totalWage);
             }
 
 
