e46a49ee44c4a1515b3493c21eb51a8216a90951
Kriti Kalpa <krtklpsh@gmail.com>
2020-09-28 11:24:31 +0530

    [Kriti] Add. Modified to switch case statement

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -4,10 +4,11 @@ namespace EmployeeWage
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
+            const int IS_PART_TIME = 1;
+            const int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
 
             int empHours = 0;
@@ -15,18 +16,21 @@ namespace EmployeeWage
             Random random = new Random();
 
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_PART_TIME)
+            switch (empCheck)
             {
-                empHours = 4;
+                case IS_PART_TIME:
+                    empHours = 4;
+                    break;
+
+                case IS_FULL_TIME:
+                    empHours = 8;
+                    break;
+
+                default:
+                    empHours = 0;
+                    break;
             }
-            else if (empCheck == IS_FULL_TIME)
-            {
-                empHours = 8;
-            }
-            else
-            {
-                empHours = 0;
-            }
+
             empWage = empHours * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage : " + empWage);
         }
